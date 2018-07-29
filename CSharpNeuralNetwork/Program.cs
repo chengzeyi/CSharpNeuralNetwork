cc7fa6e84553f27dcdab208a37f08e32ddd3abb4
C <ichengzeyi@gmail.com>
2018-07-29 10:23:08 +0800

    添加项目文件。

---
+++ CSharpNeuralNetwork/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CSharpNeuralNetwork
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
